a9b05a3b89e003d0a2f7db399ff8adbabc34a884
Jdoing <703627622@qq.com>
2014-12-27 14:22:17 +0800

    add dowhile function

---
+++ dowhile.s
@@ -0,0 +1,41 @@
+.section .data
+		n: .int 3
+		b: .int 1057
+        format: .asciz "%d\n"
+.section .text
+.global _start
+_start:
+		pushl %ebp
+		movl %esp, %ebp
+		subl $8, %esp
+		
+		movl n, %ecx		
+		movl %ecx, (%esp)
+		call fact_do
+		
+		
+		pushl %eax
+        pushl $format
+        call printf
+        movl $0, (%esp)
+        call exit
+		
+fact_do:
+		pushl %ebp
+		movl %esp, %ebp
+		pushl %ebx
+		
+		movl 8(%ebp), %edx	#get n
+		movl $1, %eax		#init result
+.loop:
+		imul %edx, %eax
+		dec %edx
+		cmp $1, %edx
+		jg .loop
+		
+		popl %ebx
+		popl %ebp
+		
+		ret
+	
+		
