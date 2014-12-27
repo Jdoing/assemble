5857593fed6501d4080981c16d85b2de2a68cecf
Jdoing <703627622@qq.com>
2014-12-27 17:30:31 +0800

    add printf array function

---
+++ parray.s
@@ -0,0 +1,59 @@
+.section .data
+		array: .int 3, 0, 5, 1, 4, 6, 2, 9, 8, 7
+		len: .int 10
+        format: .asciz "%d\n"
+.section .text
+.global _start
+_start:
+		pushl %ebp
+		movl %esp, %ebp
+		subl $20, %esp	#allocate space
+		
+		movl $array, %edx
+		movl %edx, (%esp)	#store &array on the stack
+		
+		movl len, %ecx
+		movl %ecx, 4(%esp)	#store len on the stack
+		
+		call parray
+        
+        addl $20, %esp
+        popl %ebp
+
+        movl $0, (%esp)	#deallocate space
+        call exit
+parray:
+		pushl %ebp
+		movl %esp, %ebp
+		push %ebx		
+		subl $20, %esp	#allocate space
+		
+		movl 8(%ebp), %edx	#get &array
+		movl 12(%ebp), %ebx	#get len
+		
+		movl $0, %ecx
+		cmp %ecx, %ebx
+		jle .done
+.loop:
+		movl (%edx, %ecx, 4), %eax
+		pushl %edx
+		pushl %ecx
+		
+		pushl %eax
+        pushl $format
+        call printf
+		
+		addl $8, %esp
+		popl %ecx
+		popl %edx
+		
+		inc %ecx
+		cmp %ecx, %ebx
+		jg .loop		
+.done:
+		addl $20, %esp
+		popl %ebx
+		popl %ebp
+		
+		ret
+		
